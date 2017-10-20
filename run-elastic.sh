2c8caf8d0c53c8aa57d446bb43fd02fa684df63b
Nien Nguyen <nguyendinhnien@gmail.com>
2017-10-20 10:19:14 +0700

    refactor filename tuning run file

--- run-elastic.sh
+++ run-elastic.sh
@@ -6,12 +6,26 @@ kubectl create -f es-svc.yaml
 kubectl create -f es-master.yaml
  
 # Check to see if the deployments are running
-sleep 15
+while true; do
+    active=`kubectl get deployments --all-namespaces | grep es-master | awk '{print $6}'`
+    if [ "$active" == "1" ]; then
+    break
+    fi
+    sleep 2
+done
 
 kubectl create -f es-client.yaml
 kubectl create -f es-data-svc.yaml
 kubectl create -f es-data-stateful.yaml
 
+while true; do
+    active=`kubectl get deployments --all-namespaces | grep es-client | awk '{print $6}'`
+    if [ "$active" == "1" ]; then
+    break
+    fi
+    sleep 2
+done
+ 
 # Scale the cluster to 3 master, 4 data, and 2 client nodes
 kubectl scale deployment es-master --replicas 3
 kubectl scale deployment es-client --replicas 2
